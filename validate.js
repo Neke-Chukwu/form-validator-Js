d3b710ae32b8e41ccec68dc52d6491bbe055c87f
Muoebonam Chukwuneke <nekeagust@gmail.com>
2025-01-21 18:46:12 +0100

    Add JavaScript validation for contact form

---
+++ validate.js
@@ -0,0 +1,15 @@
+function checkdata()
+    {
+        var username = document.getElementById("name");
+        var emailid = document.getElementById("email");
+    }
+    if(username.value==""){
+        alert("Please enter your name");
+        username.focus();
+        return false;
+    }
+    if(emailid.value==""){
+        alert("Please enter your email");
+        emailid.focus();
+        return false;
+    }
